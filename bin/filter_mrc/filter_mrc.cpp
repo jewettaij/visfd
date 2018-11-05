cb6a40e2dc7fac1630d33f77c27dc511dc7f73ed
Andrew Jewett <jewett.aij@gmail.com>
2018-11-05 14:09:46 -0800

    updated version number in filter_mrc.cpp

--- bin/filter_mrc/filter_mrc.cpp
+++ bin/filter_mrc/filter_mrc.cpp
@@ -30,8 +30,8 @@ using namespace std;
 
 
 string g_program_name("filter_mrc.cpp");
-string g_version_string("0.9.0");
-string g_date_string("2018-11-02");
+string g_version_string("0.9.1");
+string g_date_string("2018-11-05");
 
 
 
