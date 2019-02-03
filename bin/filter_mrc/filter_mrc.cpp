5625517075aea83d727144ce17678ed802226eba
Andrew Jewett <jewett.aij@gmail.com>
2019-02-02 22:20:32 -0800

    updated version info

--- bin/filter_mrc/filter_mrc.cpp
+++ bin/filter_mrc/filter_mrc.cpp
@@ -33,8 +33,8 @@ using namespace std;
 
 
 string g_program_name("filter_mrc");
-string g_version_string("0.13.5");
-string g_date_string("2018-1-29");
+string g_version_string("0.13.6");
+string g_date_string("2018-2-02");
 
 
 
