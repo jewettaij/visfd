01d8ed1fd19b0fa4f3878d3ae14287dec1221c20
Andrew Jewett <jewett.aij@gmail.com>
2019-03-30 10:15:09 -0700

    updated "convert_to_float"

--- bin/convert_to_float/convert_to_float.cpp
+++ bin/convert_to_float/convert_to_float.cpp
@@ -7,28 +7,39 @@ using namespace std;
 
 
 int main(int argc, char **argv) {
-  if (argc <= 1) {
-    cerr << "This program expects at an argument: input_file [output_file]\n"
-         << "No tomogram file specified.  Exiting.\n" << endl;
-    exit(0);
+  if (argc <= 2) {
+    cerr << "\n"
+         << "ERROR: This program expects two arguments:  input_file  output_file\n"
+         << endl;
+    exit(1);
   }
 
   try {
     // Read the file
     string in_file_name(argv[1]);
+    string out_file_name(argv[2]);
     cerr << "Reading tomogram \""<<in_file_name<<"\"" << endl;
     MrcSimple tomo;
     tomo.Read(in_file_name);//You can also use "tomo.Read(cin);" or "cin>>tomo;"
     tomo.PrintStats(cerr);  //Optional (display the tomogram size & format)
-    WarnMRCSignedBytes(tomo, in_file_name, cerr);
 
-    // Write the file
-    if (argc > 2) {
-      string out_file_name(argv[2]);
-      cerr << "writing tomogram (in float mode)" << endl;
-      tomo.Write(out_file_name); // You can also use "tomo.Write(cout);" 
-                                 // or "cout<<tomo;"
+    if (tomo.header.use_signed_bytes &&
+      tomo.header.mode == MrcHeader::MRC_MODE_BYTE) {
+      cerr <<
+        "\n"
+        "WARNING: File \""<< in_file_name <<"\"\n"
+        "         uses signed bytes.\n"
+        "         IMOD programs (such as 3dmod) will probably add 128 to each voxel's\n"
+        "         brightnesses to insure that they are all positive.\n"
+        "         This program does not do that.  If, for some reason, you\n"
+        "         want to insure that all brightnesses are positive, use this instead:\n"
+        " newstack -mode 2 -in "<<in_file_name<<" -ou "<<out_file_name<<"\n"
+                          << endl;
     }
+
+    cerr << "writing tomogram (in float mode)" << endl;
+    tomo.Write(out_file_name); // You can also use "tomo.Write(cout);" 
+                               // or "cout<<tomo;"
   }
   catch (string s) {
     cerr << s << endl; // In case of file format error, print message and exit
