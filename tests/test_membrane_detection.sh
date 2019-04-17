5d51d49a5872a1f5e11e9cb47a25b8b01159dfdb
Andrew Jewett <jewett.aij@gmail.com>
2019-04-17 14:09:29 -0700

    fixed unit test (again) and updated version info

--- tests/test_membrane_detection.sh
+++ tests/test_membrane_detection.sh
@@ -7,7 +7,7 @@ test_membrane_detection() {
     OUT_FNAME_NORMALS=${OUT_FNAME_BASE}.ply
     ../bin/filter_mrc/filter_mrc -w 19.2 -i test_image_membrane.rec -out ${OUT_FNAME_REC} -surface minima 55 -surface-tv 4 -surface-tv-angle-exponent 4 -connect-vector-saliency 0.707 -connect-vector-neighbor 0.707 -connect-tensor-saliency 0.707 -connect-tensor-neighbor 0.707 -connect 1e+09 -surface-normals-file ${OUT_FNAME_NORMALS} -select-cluster 1 >& test_log_e.txt
     assertTrue "Failure during membrane detection, tensor-voting, or voxel clustering:  File \"${OUT_FNAME_REC}\" not created" "[ -s ${OUT_FNAME_REC} ]"
-    N_CLUSTERS=`grep 'Number of clusters found:' < test_log_e.txt | awk '{print $6}'`
+    N_CLUSTERS=`grep 'Number of clusters found:' < test_log_e.txt | awk '{print $5}'`
     assertTrue "Failure: No membrane clusters detected." "[ $N_CLUSTERS -gt 0 ]"
     SUM_VOXELS=`../bin/sum_voxels/sum_voxels "${OUT_FNAME_REC}"`
     assertTrue "Failure during membrane detection. File \"${OUT_FNAME_REC}\" has a weird checksum." "[ $SUM_VOXELS -gt 6000 ]"
