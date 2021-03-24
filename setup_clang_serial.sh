3a67dd4140ed7d145a3eadff73e8ef3c012e6519
Andrew Jewett <jewett.aij@gmail.com>
2021-03-24 15:10:08 -0700

    updated a compiler setting in the "setup_clang_serial.sh" file

--- setup_clang_serial.sh
+++ setup_clang_serial.sh
@@ -7,7 +7,7 @@ export L_COMP="ar rs"
 export LFLAGS=""  #-static
 
 export MY_FLAGS="-std=c++11 -O3 -DNDEBUG -ffast-math -DDISABLE_OPENMP"
-export CFLAGS="-c $MY_FLAGS -fopenmp"
+export CFLAGS="-c $MY_FLAGS"
 export CPP_PRELINKER_COMMAND="echo"
 export COMPILER_TEMP_FILES=""
 export LINKER_TEMP_FILES=""
